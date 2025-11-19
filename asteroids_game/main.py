78f1ea4a06a0a2c0d31e9469552e90ba42b3e54e
or614 <oliross2000@gmail.com>
2025-11-19 16:45:43 +0000

    B: Added logger and constants and created gitignore to ignore logs

---
+++ asteroids_game/main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
