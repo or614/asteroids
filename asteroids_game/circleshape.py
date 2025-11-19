70b36461c65db54058199e1c57a170a0581fde98
or614 <oliross2000@gmail.com>
2025-11-19 17:25:44 +0000

    D: Added player onto screen and added classes for player and circles

---
+++ asteroids_game/circleshape.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import LINE_WIDTH
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        points = self.triangle()
+        pygame.draw.polygon(screen, "white", points, LINE_WIDTH)
+        pass
+
+    def update(self, dt):
+        # must override
+        pass
--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -1,26 +1,44 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
-pygame.init()
+
 
 def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
     
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     loop = True
     while loop == True:
         log_state()
+
+        
+        
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        
+
+        dt = clock.tick(60)/1000
+
+
     
     
 
